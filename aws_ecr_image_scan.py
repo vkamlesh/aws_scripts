572c9af6c2b9e1f31e0ed6438126c78497717cd9
vkamlesh <kamlesh.a.verma@hotmail.com>
2020-05-05 11:22:06 +0530

    add argument for --exclude_repo

--- aws_ecr_image_scan.py
+++ aws_ecr_image_scan.py
@@ -18,18 +18,24 @@ args = parser.parse_args()
 session = boto3.session.Session(profile_name=args.profile)
 client = session.client('ecr', region_name='ap-south-1')
 
-if len(args.repo) > 0:
-    print(f"this is value for {args.repo}")
-    response = client.describe_repositories(registryId=args.account_id, repositoryNames=args.repo)
-else:
+repo_list = []
+image_list = []
+
+if args.repo is None:
     response = client.describe_repositories(registryId=args.account_id)
+    exclude_repo = args.exclude_repo
+else:
+    response = client.describe_repositories(registryId=args.account_id, repositoryNames=args.repo)
+    exclude_repo = []
+
 
 for i in response['repositories']:
     repo_name = i['repositoryName']
     ecr_latest = client.list_images(
         registryId=args.account_id, repositoryName=repo_name, maxResults=1, filter={'tagStatus': 'TAGGED'})
-    if len(ecr_latest['imageIds']) != 0  repo_name not in args.exclude_repo:
-        print(f"\nRepo name: {repo_name} \n\n First_Image: {ecr_latest}\n\n")
-    # print(i['repositoryArn'], i['repositoryName'])
+    if len(ecr_latest['imageIds']) != 0 and (not exclude_repo or repo_name not in exclude_repo):
+        image_list.append([repo_name, ecr_latest['imageIds'][0]['imageDigest'],
+                           ecr_latest['imageIds'][0]['imageTag']])
 
-    # print(ecr_latest['imageIds'][0]['imageTag'])
+
+def image_scan():
