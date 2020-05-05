a3de41770b92bff374a7756acf554ff697513845
vkamlesh <kamlesh.a.verma@hotmail.com>
2020-05-05 19:02:02 +0530

    scan findings

--- aws_ecr_image_scan.py
+++ aws_ecr_image_scan.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import boto3.session
 import argparse
+import boto3.session
+import botocore.exceptions
+
 
 parser = argparse.ArgumentParser()
 
@@ -12,6 +14,8 @@ parser.add_argument('--repo', nargs='+',
                     help='A list of repositories to describe. If this parameter is omitted, then all repositories in a registry are described.', type=str)
 
 parser.add_argument('--exclude_repo', nargs='+', help='A list of exclude repositories.', type=str)
+
+parser.add_argument('--result', help='Send result to given Email id', type=str)
 
 args = parser.parse_args()
 
@@ -38,4 +42,36 @@ for i in response['repositories']:
                            ecr_latest['imageIds'][0]['imageTag']])
 
 
-def image_scan():
+def image_scan(*args):
+    for repo_name, image_id, image_tag in args:
+        try:
+            response = client.start_image_scan(repositoryName=repo_name, imageId={
+                                               'imageDigest': image_id, 'imageTag': image_tag})
+        except botocore.exceptions.ClientError as error:
+            print(
+                f"An image scan can only be started once per day on an individual image.\nExcepation: {error}")
+            exit()
+
+    return response['imageScanStatus']
+
+
+def scan_result(*args):
+    wait_scan = []
+    failed_scan = []
+    for repo_name, image_id, image_tag in args:
+        response = client.describe_image_scan_findings(repositoryName=repo_name, imageId={
+                                                       'imageDigest': image_id, 'imageTag': image_tag})
+        if response['imageScanStatus'] == 'COMPLETE':
+            print(response['imageScanFindings']['imageScanCompletedAt'])
+            print(response['imageScanFindings']['vulnerabilitySourceUpdatedAt'])
+            print(response['imageScanFindings']['findings'])
+        elif response['imageScanStatus'] == 'IN_PROGRESS':
+            wait_scan.append([repo_name, image_id, image_tag])
+        elif response['imageScanStatus'] == 'FAILED':
+            failed_scan.append([repo_name, image_id, image_tag])
+
+
+if args.result True:
+    scan_result(image_list)
+    print(wait_scan)
+    print(failed_scan)
